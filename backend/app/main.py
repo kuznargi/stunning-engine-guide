09e1a5cd2b6ee888f2bb4381ca27b7ece58b63df
kuznargi <kuzybahovanargiza@gmail.com>
2025-11-06 15:07:34 +0500

    changed main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -804,4 +804,4 @@ def api_route_compare(req: RouteCompareRequest) -> Dict[str, Any]:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080)
